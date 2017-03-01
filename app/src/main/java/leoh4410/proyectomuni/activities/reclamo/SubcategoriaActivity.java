58fe0849d5836f095ce03a221ffdf01ea3a103b9
leo4444 <leopoldohenchoz@hotmail.com>
2017-03-01 18:58:29 -0300

    iconos categoria

---
+++ app/src/main/java/leoh4410/proyectomuni/activities/reclamo/SubcategoriaActivity.java
@@ -0,0 +1,15 @@
+package leoh4410.proyectomuni.activities.reclamo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import leoh4410.proyectomuni.R;
+
+public class SubcategoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subcategoria);
+    }
+}
